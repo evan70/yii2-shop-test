a1f5b19f1a62500c315eb2499f4f435fdab4d0c8
evan70 <ivan.paldan@yandex.com>
2020-12-14 23:11:46 +0100

    init config

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -8,8 +8,17 @@ class TestController extends Controller
 {
     public $defaultAction = 'my-test';
 
-    public function actionIndex()
+    public function actions()
     {
+        return [
+            // объявляет "test" действие с помощью названия класса
+            'test' => 'app\components\HelloAction',
+        ];
+    }
+
+    public function actionIndex($name, $age = 25)
+    {
+        var_dump($name, $age);
         return '<h2>Hello world!</h2>';
     }
 
